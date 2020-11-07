4e63364fcc2981d2552d9da4701cc5a6e657cde8
Kenneth <kyleabs20@gmail.com>
2020-11-06 16:42:38 -0800

    demo trial working

--- index.js
+++ index.js
@@ -14,16 +14,29 @@ const io = socketio(server)
 
 // socket connection and disconection
 io.on('connection', (socket) => {
-    console.log("user connected");
-
     socket.on('join', ({name, room}, callback)=>{
-        const {error, user}  = addUser({id, name, room})
+        const {error, user}  = addUser({id: socket.id, name, room})
+        console.log(name, room)
 
         if (error) return {error}
 
-        socket.emit('message', {user: "admin", text: `${name}!!! welcome senior most boss to room ${room}`})
+        // this is imforming the user that he is welcome to the chat 
+        socket.emit('message', {user: "admin", text: `${user.name}!!! welcome senior most boss to room ${user.room}`})
+
+        // making a broadcast to everyone else about a user's entry 
+        socket.broadcast.to(user.room).emit('message', {user: 'admin', text: `${user.name} has just joined the partyyyyy`})
 
         socket.join(user.room)
+
+        callback()
+    })
+
+    socket.on('sendMessage', (message, callback)=>{
+        const user = getUser(socket.id)
+        // console.log(user)
+        io.to(user.room).emit('message', {user: user.name, text: message})
+
+        callback()
     })
 
     socket.on('disconnect', ()=>{
